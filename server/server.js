27533efd4c0aee5665ee786a4a77d97859aed813
jsagayaraj <bjsahay@gmail.com>
2018-07-06 13:05:43 +0200

    first commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express')
+
+const publicPath = path.join(__dirname, '../public');
+
+//Heroku port setup
+const prot = process.env.PORT || 3000.
+
+
+const app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(3000, ()=>{
+  console.log(`Server is up on ${port}`);
+})
